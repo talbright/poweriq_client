f92b1ecf6ad3a42f2da4a02e9e0ed1e576eabaad
Trent Albright <trent.albright@gmail.com>
2011-06-27 11:21:27 -0400

    services for asset strips and rack units

--- lib/poweriq/rack_unit.rb
+++ lib/poweriq/rack_unit.rb
@@ -1,6 +1,10 @@
 module PowerIQ
 
   class RackUnit < Resource::Base
+     
+     resource_action :index
+     resource_action :show
+     resource_action :update
           
      resource_accessor  :asset_strip_id,:tag_id,:created_at,:updated_at,
                         :ordinal,:led_state,:led_mode,:led_color
--- spec/asset_strip_spec.rb
+++ spec/asset_strip_spec.rb
@@ -11,12 +11,12 @@ describe PowerIQ::AssetStrip do
   it "should perform action 'show'" do
     a = PowerIQ::AssetStrip.new
     a.id=1
-    a.show(:params=>{:p1=>1,:p2=>2})
+    a.show
   end
 
   it "should perform action 'index'" do
     a = PowerIQ::AssetStrip.new
-    data = a.index(:params=>{:p1=>1,:p2=>2})
+    a.index
   end
   
 end
