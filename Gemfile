f79e1b7599ec950dff3049bb74f3839442835d31
Trent Albright <trent.albright@gmail.com>
2011-10-30 20:09:34 -0400

    cleanup

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source :rubygems
-
-gem "rest-client"
-# gem "activemodel", "3.0.9"
--- lib/poweriq.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
-
-require 'active_support/core_ext'
-require 'poweriq/configuration'
-require 'poweriq/resources'
-
-module PowerIQ
-  extend Configuration
-end
