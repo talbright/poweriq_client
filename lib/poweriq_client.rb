d59240cea42882fddde0ea8b7c996e00e2f351c7
Trent Albright <trent.albright@gmail.com>
2011-11-02 20:18:50 -0400

    compatability checking

--- lib/poweriq_client.rb
+++ lib/poweriq_client.rb
@@ -3,6 +3,7 @@ require 'active_support/core_ext'
 
 module PowerIQ
   autoload :Version, "poweriq_client/version"
+  autoload :Compatability, "poweriq_client/compatability"
   autoload :Resource, "poweriq_client/resource"
   autoload :Configuration, "poweriq_client/configuration"
 end
