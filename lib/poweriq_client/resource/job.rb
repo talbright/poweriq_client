2aaf5f10cf36ff234bb7f4d1e84265cf403ade2a
Trent Albright <trent.albright@gmail.com>
2011-11-07 13:37:28 -0500

    added job polling to Job resource

--- lib/poweriq_client/resource/job.rb
+++ lib/poweriq_client/resource/job.rb
@@ -1,6 +1,37 @@
 module PowerIQ
   module Resource
-    class Job < PowerIQ::Resource::Base; end
+    class Job < PowerIQ::Resource::Base
+
+      def poll(options={:delay=>5,:max=>12})
+        require_member!
+        options.symbolize_keys!
+        poll_count = 0
+        unless(completed? || poll_count < options[:max])
+          poll_count += 1
+          get
+          Kernel.sleep(options[:delay])
+        end
+        begin
+          messages_json = Job.new(url+"/messages").get
+          self.response.json.merge!(messages_json)
+        rescue
+        end
+        completed? == true
+      end
+
+      def completed?
+        require_member!
+        require_json!
+        self.response.json[root_key]["completed"] == true
+      end
+
+      def has_errors?
+        require_member!
+        require_json!
+        self.response.json[root_key]["has_errors"] == true
+      end
+
+    end
   end
 end
 
